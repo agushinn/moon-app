4ee1748807750632ed696339569671d9dd471c72
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-01-17 11:47:05 -0300

    create NotFound page

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,24 @@
+import styles from '@styles/pages/NotFound/NotFound.module.scss'
+
+import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
+
+import { Header } from '@components/Header/Header'
+import { LanguageSwitcher } from '@components/LanguageSwitcher/LanguageSwitcher'
+
+const NotFound = () => {
+    const { t } = useTranslation()
+    return (
+        <section className={styles.notFoundSection}>
+            <LanguageSwitcher />
+            <Header />
+            <h4 className={styles.notFoundTitle}>{t('not-found-title')}</h4>
+            <div className={styles.moonContainer}>:(</div>
+            <Link className={styles.notFoundLink} to="/">
+                {t('not-found-link')}
+            </Link>
+        </section>
+    )
+}
+
+export { NotFound }
