095efa42ae3a0e95a0541a607c232924ace03b4b
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-01-17 11:40:28 -0300

    create LunarPhase component

---
+++ src/components/LunarPhase/LunarPhase.jsx
@@ -0,0 +1,28 @@
+import { useSelector } from 'react-redux'
+
+import styles from '@styles/components/LunarPhase/LunarPhase.module.scss'
+import { HEMISPHERE } from '@utils/Moon'
+
+import { useTranslation } from 'react-i18next'
+
+const LunarPhase = ({ name, icon: Icon, className = '' }) => {
+    const { t } = useTranslation()
+
+    const hemisphere = useSelector(
+        (state) => state.hemisphere.currentHemisphere
+    )
+    return (
+        <div className={`${className} ${styles.lunarPhaseContainer} `}>
+            <Icon
+                className={`${styles.lunarPhaseIcon} ${
+                    hemisphere === HEMISPHERE.SOUTHERN
+                        ? styles.southern
+                        : styles.northern
+                }`}
+            />
+            <h3>{t(`${name.replace(' ', '-').toLowerCase()}-phase`)}</h3>
+        </div>
+    )
+}
+
+export { LunarPhase }
