9da2e79c086ac17d13bf726cbdd4058be4129644
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-01-17 11:57:09 -0300

    create and configure store and hemisphere and language reducers

---
+++ src/store/hemisphere/hemisphereSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+import { HEMISPHERE } from '@utils/Moon'
+
+const initialState = {
+    currentHemisphere:
+        localStorage.getItem('STORED_HEMISPHERE') || HEMISPHERE.NORTHERN,
+}
+
+const hemisphereSlice = createSlice({
+    name: 'hemisphere',
+    initialState,
+    reducers: {
+        setHemisphere: (state, action) => {
+            state.currentHemisphere = action.payload
+            localStorage.setItem('STORED_HEMISPHERE', action.payload)
+        },
+    },
+})
+
+export const hemisphereActions = hemisphereSlice.actions
+export default hemisphereSlice.reducer
