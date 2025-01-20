e56930750abfadf047196cb13ac611bc14d118d6
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-01-20 16:09:42 -0300

    fix typo and test speed insights

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { speedInsights } from '@vercel/speed-insights/plugin'
+import speedInsights from '@vercel/speed-insights/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
